18ea1e6f76e414ab44e63dfc5741ed1f9664c05b
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2024-04-07 22:06:12 +0200

    add cards

--- sveltekit/src/routes/courses/[courseTitle]/chapters/[chapterTitle]/+layout.svelte
+++ sveltekit/src/routes/courses/[courseTitle]/chapters/[chapterTitle]/+layout.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
+    import {Separator} from "$lib/components/ui/separator";
+
     export let data;
 </script>
 
-<div class="flex flex-row">
+<div class="flex flex-row items-stretch">
     <div class="basis-1/4">
         <h2 class="text-2xl font-bold">Chapters</h2>
         {#if data.chapters.length > 0}
             <ul>
                 {#each data.chapters as chapter}
-                    {#if chapter.active}
-                        <li>
-                            <a href={`/courses/${chapter.Course.Title}/chapters/${chapter.Title}`} class="font-bold">{chapter.Title}</a>
-                        </li>
-                    {:else}
-                        <li>
-                            <a href={`/courses/${chapter.Course.Title}/chapters/${chapter.Title}`}>{chapter.Title}</a>
-                        </li>
-                    {/if}
+                    <li>
+                        <a href={`/courses/${chapter.Course.Title}/chapters/${chapter.Title}`} class={chapter.active ? 'font-bold' : ''}>{chapter.Title}</a>
+                    </li>
                 {/each}
             </ul>
         {:else}
             <p>No chapters available for this course.</p>
         {/if}
     </div>
+    <!-- Separator with Tailwind CSS for visible styling -->
+    <div class="w-px bg-gray-300 mx-4"></div>
     <div class="basis-3/4">
         <slot/>
     </div>

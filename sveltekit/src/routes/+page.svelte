674c2b9e601615f9c8da3f24104018ee0a31d92f
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-05-08 23:26:09 +0200

    bring in the text

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,7 +1,23 @@
-<div class="relative">
+<div class="relative w-full h-[60vh] flex">
     <!-- Gradient Background -->
     <div class="absolute inset-0 bg-gradient-to-br from-40% from-blue-500 to-90% to-purple-500 opacity-30"></div>
 
     <!-- Image Overlay -->
     <img src="http://localhost:5173/grainy.png" alt="Overlay" class="mix-blend-overlay w-full h-full">
+
+    <!-- Text Overlay -->
+    <div class="absolute left-1/4 max-w-3/4 space-y-64 pt-8">
+        <h1 class="text-6xl font-bold uppercase text-primary">LACE</h1>
+        <div class="space-y-4">
+            <div class="space-y-2">
+                <p class="text-6xl font-bold">Learn. Apply. Comply.</p>
+                <p class="text-6xl font-bold">Continuing Education Materials on </p>
+                <p class="text-6xl font-bold">Privacy-Enhancing Technologies</p>
+            </div>
+            <div class="space-y-2">
+                <p class="text-3xl">Support your organization in adapting</p>
+                <p class="text-3xl">to Privacy Enhancing Technologies</p>
+            </div>
+        </div>
+    </div>
 </div>

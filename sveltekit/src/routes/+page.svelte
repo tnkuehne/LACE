08403db056e2135511caa587b3d053abe50cd3f4
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-05-08 23:02:11 +0200

    add background image

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="relative">
+    <!-- Gradient Background -->
+    <div class="absolute inset-0 bg-gradient-to-br from-40% from-blue-500 to-90% to-purple-500 opacity-30"></div>
+
+    <!-- Image Overlay -->
+    <img src="http://localhost:5173/grainy.png" alt="Overlay" class="mix-blend-overlay w-full h-full">
+</div>

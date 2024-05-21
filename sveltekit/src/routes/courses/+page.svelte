5cc1beb93751243c5722cb647d24ff0b11f9ca0f
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-05-21 18:38:56 +0200

    make responsive

--- sveltekit/src/routes/courses/+page.svelte
+++ sveltekit/src/routes/courses/+page.svelte
@@ -4,7 +4,7 @@
 	export let data;
 </script>
 
-<div class="bg-blueGray-50 min-h-screen">
+<div class="bg-blueGray-50 min-h-screen px-4">
 	<div class="mx-auto max-w-screen-2xl">
 		<h1
 			class="md:text-6x font-sansation text-4xl font-bold uppercase lining-nums tabular-nums leading-none tracking-[0.12em] text-blue-800"
@@ -12,9 +12,9 @@
 			LACE
 		</h1>
 		<h2 class="text-2xl font-bold">My Courses</h2>
-		<div class="-mx-2 flex flex-row flex-wrap justify-center">
+		<div class="-mx-2 flex flex-row flex-wrap justify-center space-y-4 md:space-y-0">
 			{#each data.courses as course}
-				<div class="w-full px-2 sm:w-1/2 lg:w-1/3">
+				<div class="w-full px-2 md:w-1/3">
 					<CourseCard
 						{course}
 						chapters={data.chapters.filter((chapter) => chapter.kurs.Title === course.Title)}

a29b54e395218a2ba86eb5fb0b9b620e1b849d0e
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2024-05-28 19:51:06 +0200

    75 simple analytics (#78)
    * add proxy for post requests
    
    * add tracking utility
    
    * update directus
    
    * migrate to postgres
    
    * move post to api endpoint to retrieve ip address
    
    * log device, browser and os

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -2,6 +2,31 @@
 	import '../app.pcss';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { ModeWatcher } from 'mode-watcher';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	async function trackPageView(path, referrer) {
+		await fetch('/api/analytics', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ path, referrer })
+		});
+	}
+
+	onMount(() => {
+		const referrer = document.referrer;
+		const path = $page.url.href;
+
+		// For testing purposes
+		trackPageView(path, referrer);
+
+		// Only track if referrer is different from the current hostname
+		if (referrer && new URL(referrer).hostname !== $page.url.hostname) {
+			trackPageView(path, referrer);
+		}
+	});
 </script>
 
 <Toaster />

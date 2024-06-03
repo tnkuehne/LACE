7423d4c8eb524ad5c73f48d99a72b93de39370ae
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2024-06-03 16:04:50 +0200

    add preview mode (#88)

--- sveltekit/src/hooks.server.ts
+++ sveltekit/src/hooks.server.ts
@@ -1,8 +1,10 @@
-import type { Handle } from '@sveltejs/kit'; // Import the Handle type from SvelteKit
-// Import the Handle type from SvelteKit
+import type { Handle } from '@sveltejs/kit';
+import { PREVIEW_COOKIE_NAME, PREVIEW_SECRET } from '$env/static/private';
 
 // Define the function with the appropriate types
 export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.previewMode = event.cookies.get(`${PREVIEW_COOKIE_NAME}`) === PREVIEW_SECRET;
+
 	return resolve(event, {
 		filterSerializedResponseHeaders: (key: string, value: string) => {
 			return key.toLowerCase() === 'content-type';

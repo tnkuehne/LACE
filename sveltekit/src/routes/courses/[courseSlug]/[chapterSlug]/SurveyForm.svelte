2de18e8c39b224e8e943fd1dc45a643d3f33ac11
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-06-30 00:06:52 +0200

    add select option and sections

--- sveltekit/src/routes/courses/[courseSlug]/[chapterSlug]/SurveyForm.svelte
+++ sveltekit/src/routes/courses/[courseSlug]/[chapterSlug]/SurveyForm.svelte
@@ -2,6 +2,7 @@
 	import { Button } from '$lib/components/ui/button';
 	import { Label } from '$lib/components/ui/label';
 	import { Textarea } from '$lib/components/ui/textarea';
+	import * as Select from '$lib/components/ui/select';
 	import { enhance } from '$app/forms';
 
 	export let questions;
@@ -10,6 +11,18 @@
 	export let courseId: string;
 
 	const likertScale = ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree'];
+
+	let groupedQuestions = questions.reduce((grouped, question) => {
+		(grouped[question.section] = grouped[question.section] || []).push(question);
+		return grouped;
+	}, {});
+
+	let sections = Object.keys(groupedQuestions);
+	let selectedValues = {};
+
+	function handleSelectChange(question, value) {
+		selectedValues[question] = value;
+	}
 </script>
 
 <form
@@ -18,33 +31,58 @@
 	method="POST"
 	use:enhance
 >
-	{#each questions as question}
-		{#if question.course === course || question.course == null}
-			<div>
-				<Label>{question.question}</Label>
-				{#if question.type === 'open'}
-					<Textarea name={question.question} required={question.required} />
-				{/if}
-				{#if question.type === 'likert'}
-					<div class="flex flex-col pt-2">
-						<div class="flex flex-row justify-between">
-							{#each likertScale as option}
-								<input
-									type="radio"
-									name={question.question}
-									value={option}
-									required={question.required}
-								/>
-							{/each}
-						</div>
-						<div class="flex flex-row justify-between">
-							<span class="text-sm">Strongly agree</span>
-							<span class="text-sm">Strongly disagree</span>
-						</div>
+	{#each sections as section}
+		<div>
+			<h2 class="font-medium">{section}</h2>
+			{#each groupedQuestions[section] as question}
+				{#if question.course === course || question.course == null}
+					<div class="pt-4">
+						<Label>{question.question}</Label>
+						{#if question.type === 'open'}
+							<Textarea name={question.question} required={question.required} />
+						{/if}
+						{#if question.type === 'likert'}
+							<div class="flex flex-col pt-2">
+								<div class="flex flex-row justify-between">
+									{#each likertScale as option}
+										<label>
+											<input
+												type="radio"
+												name={question.question}
+												value={option}
+												required={question.required}
+											/>
+											{option}
+										</label>
+									{/each}
+								</div>
+								<div class="flex flex-row justify-between">
+									<span class="text-sm">Strongly agree</span>
+									<span class="text-sm">Strongly disagree</span>
+								</div>
+							</div>
+						{/if}
+						{#if question.type.includes(';')}
+							<Select.Root onSelectedChange={(e) => handleSelectChange(question.question, e.value)}>
+								<Select.Trigger class="w-[180px]">
+									<Select.Value placeholder="Options" />
+								</Select.Trigger>
+								<Select.Content>
+									{#each question.type.split(';') as option}
+										<Select.Item value={option}>{option}</Select.Item>
+									{/each}
+								</Select.Content>
+							</Select.Root>
+							<input
+								type="hidden"
+								name={question.question}
+								value={selectedValues[question.question] || ''}
+							/>
+						{/if}
 					</div>
 				{/if}
-			</div>
-		{/if}
+			{/each}
+		</div>
 	{/each}
-	<Button type="submit">{submit_button}</Button>
+	<Button type="submit" class="light:text-white">{submit_button}</Button>
 </form>

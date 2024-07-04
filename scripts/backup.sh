2205df2d0572021032206e579b569abb76f9aef3
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-07-04 23:10:42 +0200

    refine

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,14 +6,13 @@ source .env
 # Set backup file name with timestamp
 BACKUP_FILE=backup_$(date +%Y%m%d%H%M%S).sql
 
-# Perform the backup
-sudo docker exec -e PGPASSWORD=$POSTGRES_PASSWORD -t $DATABASE_CONTAINER_NAME pg_dump -U $POSTGRES_USER $POSTGRES_DB > $BACKUP_FILE
-
-# ToDo: Upload the backup file to a cloud storage (e.g. LRZ Backups)
-
-if [ $? -eq 0 ]; then
+# Perform the backup and check if it was successful
+# shellcheck disable=SC2024
+if sudo docker exec -e PGPASSWORD="$POSTGRES_PASSWORD" -t "$DATABASE_CONTAINER_NAME" pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_FILE"; then
   echo "Backup $BACKUP_FILE completed successfully."
 else
   echo "Backup failed." >&2
   exit 1
 fi
+
+# ToDo: Upload the backup file to a cloud storage (e.g., LRZ Backups)

2584aa981638bcb1e91d286f30a6f3ce0f6cb2f9
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-07-04 23:32:39 +0200

    remove unnecessary comments

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Load environment variables from .env file
 source .env
 
-# Set backup file name with timestamp
 BACKUP_FILE=backup_$(date +%Y%m%d%H%M%S).sql
 
-# Perform the backup and check if it was successful
 # shellcheck disable=SC2024
 if sudo docker exec -e PGPASSWORD="$POSTGRES_PASSWORD" -t "$DATABASE_CONTAINER_NAME" pg_dump -U "$POSTGRES_USER" "$POSTGRES_DB" > "$BACKUP_FILE"; then
   echo "Backup $BACKUP_FILE completed successfully."
@@ -15,7 +12,6 @@ else
   exit 1
 fi
 
-# Compress the backup file
 gzip "$BACKUP_FILE"
 
 echo "Compressed backup to ${BACKUP_FILE}.gz completed."

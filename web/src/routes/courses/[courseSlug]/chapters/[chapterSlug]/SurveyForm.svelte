de5aa11c0d2db2a75f482671e7fa79c3b90ca561
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2024-09-03 12:44:28 +0200

    make survey multi page (#270)

--- web/src/routes/courses/[courseSlug]/chapters/[chapterSlug]/SurveyForm.svelte
+++ web/src/routes/courses/[courseSlug]/chapters/[chapterSlug]/SurveyForm.svelte
@@ -19,38 +19,67 @@
 
 	let sections = Object.keys(groupedQuestions);
 	let selectedValues = {};
+	let currentPage = 0;
 
 	function handleSelectChange(question, value) {
 		selectedValues[question] = value;
 	}
+
+	function nextPage() {
+		if (currentPage < sections.length - 1) {
+			currentPage++;
+		}
+	}
+
+	function prevPage() {
+		if (currentPage > 0) {
+			currentPage--;
+		}
+	}
+
+	function handleInputChange(event, question) {
+		selectedValues[question] = event.target.value;
+	}
 </script>
 
 <form
-	class="grid items-start gap-4 pr-4"
-	action="/courses/{course}/survey?course={courseId}"
-	method="POST"
-	use:enhance
+		class="grid items-start gap-4 pr-4"
+		action="/courses/{course}/survey?course={courseId}"
+		method="POST"
+		use:enhance
 >
-	{#each sections as section}
-		<div>
+	{#each sections as section, sectionIndex}
+		<div class:hidden={currentPage !== sectionIndex}>
 			<h2 class="font-medium underline">{section}</h2>
 			{#each groupedQuestions[section] as question}
 				{#if question.course === course || question.course == null}
 					<div class="pt-4">
-						<Label>{question.question}</Label>
+						<Label for={question.question}>{question.question}</Label>
 						{#if question.type === 'open'}
-							<Textarea name={question.question} required={question.required} />
+                            <Textarea
+									id={question.question}
+									name={question.question}
+									required={question.required}
+									value={selectedValues[question.question] || ''}
+									on:input={(e) => handleInputChange(e, question.question)}
+							/>
 						{/if}
 						{#if question.type === 'likert'}
 							<div class="flex flex-col pt-2">
 								<div class="flex flex-row justify-between">
-									{#each likertScale as option}
-										<input
-											type="radio"
-											name={question.question}
-											value={option}
-											required={question.required}
-										/>
+									{#each likertScale as option, index}
+										<div>
+											<input
+													type="radio"
+													id={`${question.question}_${index}`}
+													name={question.question}
+													value={option}
+													required={question.required}
+													checked={selectedValues[question.question] === option}
+													on:change={(e) => handleInputChange(e, question.question)}
+											/>
+											<label for={`${question.question}_${index}`} class="sr-only">{option}</label>
+										</div>
 									{/each}
 								</div>
 								<div class="flex flex-row justify-between">
@@ -71,9 +100,9 @@
 								</Select.Content>
 							</Select.Root>
 							<input
-								type="hidden"
-								name={question.question}
-								value={selectedValues[question.question] || ''}
+									type="hidden"
+									name={question.question}
+									value={selectedValues[question.question] || ''}
 							/>
 						{/if}
 					</div>
@@ -81,5 +110,13 @@
 			{/each}
 		</div>
 	{/each}
-	<Button type="submit" class="light:text-white">{submit_button}</Button>
+
+	<div class="flex justify-between mt-4">
+		<Button type="button" on:click={prevPage} disabled={currentPage === 0} class="text-secondary">Previous</Button>
+		{#if currentPage === sections.length - 1}
+			<Button type="submit" class="text-secondary">{submit_button}</Button>
+		{:else}
+			<Button type="button" on:click={nextPage} class="text-secondary">Next</Button>
+		{/if}
+	</div>
 </form>

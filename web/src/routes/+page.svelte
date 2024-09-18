d8e50239dacab7d106fab200fe482200f8836f92
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-09-18 23:45:32 +0200

    reduce code duplication and create common Footer component

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -7,6 +7,7 @@
 	import { Button } from '$lib/components/ui/button/index';
 	import CircleAlert from "lucide-svelte/icons/circle-alert";
 	import * as Alert from "$lib/components/ui/alert/index";
+	import Footer from "$lib/components/Footer.svelte";
 
 
 	export let data;
@@ -141,44 +142,4 @@
 		</section>
 	</div>
 </div>
-<footer class="bg-sky-100 dark:bg-blue-950">
-	<div
-		class="mx-auto flex max-w-screen-2xl flex-col justify-between space-y-16 px-8 py-16 md:flex-row md:space-y-0 md:px-0"
-	>
-		<div class="flex flex-col gap-8">
-			<enhanced:img src="./logo_sebis_cit.png?w=400" alt="Logo" loading="lazy" />
-			<div class="text-sm text-gray-600 dark:text-gray-400">
-				<p>Copyright © 2024 SEBIS Chair @TUM</p>
-				<p>All rights reserved</p>
-			</div>
-		</div>
-		<div class="flex flex-row gap-32">
-			<div class="space-y-4">
-				<h5 class="font-medium">Links</h5>
-				{#if data.landing?.links && data.landing.links.length > 0}
-					<ul>
-						{#each data.landing?.links as link}
-							<li>
-								<a href={link.url} class="hover:text-blue-800 dark:hover:text-blue-600"
-									>{link.title}</a
-								>
-							</li>
-						{/each}
-					</ul>
-				{:else}
-					<Alert.Root>
-						<CircleAlert class="h-4 w-4" />
-						<Alert.Title>Links Display Error</Alert.Title>
-						<Alert.Description>
-							We're currently unable to display our links. Please check back later.
-						</Alert.Description>
-					</Alert.Root>
-				{/if}
-			</div>
-			<div class="space-y-4">
-				<h5 class="font-medium">Sponsor</h5>
-				<enhanced:img src="./publisherlogo-en.svg" alt="Sponsor" loading="lazy" />
-			</div>
-		</div>
-	</div>
-</footer>
+<Footer links="{data.landing?.links}" />
--- web/src/routes/courses/+layout.svelte
+++ web/src/routes/courses/+layout.svelte
@@ -3,8 +3,7 @@
 	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb';
 	import { page } from '$app/stores';
-	import CircleAlert from 'lucide-svelte/icons/circle-alert';
-	import * as Alert from '$lib/components/ui/alert/index';
+	import Footer from "$lib/components/Footer.svelte";
 
 	export let data;
 </script>
@@ -51,44 +50,4 @@
 
 <slot />
 
-<footer class="mt-16 bg-sky-100 dark:bg-blue-950">
-	<div
-		class="mx-auto flex max-w-screen-2xl flex-col justify-between space-y-16 px-8 py-16 md:flex-row md:space-y-0 md:px-0"
-	>
-		<div class="flex flex-col gap-8">
-			<enhanced:img src="../logo_sebis_cit.png?w=400" alt="Logo" loading="lazy" />
-			<div class="text-sm text-gray-600 dark:text-gray-400">
-				<p>Copyright © 2024 SEBIS Chair @TUM</p>
-				<p>All rights reserved</p>
-			</div>
-		</div>
-		<div class="flex flex-row gap-32">
-			<div class="space-y-4">
-				<h5 class="font-medium">Links</h5>
-				{#if data.landing?.links && data.landing.links.length > 0}
-					<ul>
-						{#each data.landing?.links as link}
-							<li>
-								<a href={link.url} class="hover:text-blue-800 dark:hover:text-blue-600"
-									>{link.title}</a
-								>
-							</li>
-						{/each}
-					</ul>
-				{:else}
-					<Alert.Root>
-						<CircleAlert class="h-4 w-4" />
-						<Alert.Title>Links Display Error</Alert.Title>
-						<Alert.Description>
-							We're currently unable to display our links. Please check back later.
-						</Alert.Description>
-					</Alert.Root>
-				{/if}
-			</div>
-			<div class="space-y-4">
-				<h5 class="font-medium">Sponsor</h5>
-				<enhanced:img src="../publisherlogo-en.svg" alt="Sponsor" loading="lazy" />
-			</div>
-		</div>
-	</div>
-</footer>
+<Footer links="{data.landing?.links}" />

3927a524b0d4043f82425eaa3acf6c8f4bad6eab
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-09-25 00:27:32 +0200

    use streaming

--- web/src/routes/+page.server.ts
+++ web/src/routes/+page.server.ts
@@ -1,20 +1,14 @@
 import { readItems, readSingleton } from '@directus/sdk';
 import getDirectusInstance from '$lib/server/directus';
 import type { PageServerLoad } from './$types';
+import { json } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ fetch, url, locals }) => {
 	const version = locals.previewMode ? url.searchParams.get('version') : null;
 	const directus = getDirectusInstance(fetch);
 
-	try {
-		return {
-			courses: await directus.request(readItems('Courses', { version })),
-			landing: await directus.request(readSingleton('landing', { version, fields: ['*.*'] }))
-		};
-	} catch {
-		return {
-			courses: null,
-			landing: null,
-		}
-	}
+	return {
+		courses: directus.request(readItems('Courses')).catch(() => json(500)),
+		landing: await directus.request(readSingleton('landing', { version, fields: ['*.*'] })).catch(() => {})
+	};
 };

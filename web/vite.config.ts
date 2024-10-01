0aeb5b2af51019da57fcfb7758bbc2187e9130ab
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-10-01 17:56:13 +0200

    fix import issue

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig(async () => {

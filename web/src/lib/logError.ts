c9245f67ee7d97010e9f594df88374c6615288b3
H3nkl3r <22354443+H3nkl3r@users.noreply.github.com>
2024-10-10 17:05:25 +0200

    never fail

--- web/src/lib/logError.ts
+++ web/src/lib/logError.ts
@@ -3,19 +3,24 @@ import { env as envPublic } from '$env/dynamic/public';
 export function logError(error, event, status: number, message: string) {
 	console.log('Error logging:', error);
 	try {
-		fetch(`${envPublic.PUBLIC_WEB_URL}/api/analytics/error`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				statusCode: status,
-				stack: error.stack,
-				message,
-				event,
-				error
-			})
-		});
+		const url = envPublic.PUBLIC_WEB_URL ? `${envPublic.PUBLIC_WEB_URL}/api/analytics/error` : null;
+		if (url) {
+			fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					statusCode: status,
+					stack: error.stack,
+					message,
+					event,
+					error
+				})
+			});
+		} else {
+			console.log('PUBLIC_WEB_URL is undefined. Skipping error logging.');
+		}
 	} catch (err) {
 		console.log('Error while logging error:', err);
 	}

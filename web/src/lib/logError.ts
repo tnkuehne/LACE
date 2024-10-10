c0efc11a9a7bf040b77ff33fc4b36e5a43bc0979
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2024-10-10 17:12:54 +0200

    fix error logging (#409)
    * fix error logging
    
    * never fail

--- web/src/lib/logError.ts
+++ web/src/lib/logError.ts
@@ -1,22 +1,27 @@
 import { env as envPublic } from '$env/dynamic/public';
 
-export async function logError(error, event, status: number, message: string) {
-	console.error('Error logging:', error);
+export function logError(error, event, status: number, message: string) {
+	console.log('Error logging:', error);
 	try {
-		await fetch(`${envPublic.PUBLIC_WEB_URL}/api/analytics/error`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				statusCode: status,
-				stack: error.stack,
-				message,
-				event,
-				error
-			})
-		});
+		const url = envPublic.PUBLIC_WEB_URL ? `${envPublic.PUBLIC_WEB_URL}/api/analytics/error` : null;
+		if (url) {
+			fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					statusCode: status,
+					stack: error.stack,
+					message,
+					event,
+					error
+				})
+			});
+		} else {
+			console.log('PUBLIC_WEB_URL is undefined. Skipping error logging.');
+		}
 	} catch (err) {
-		console.error('Error while logging error:', err);
+		console.log('Error while logging error:', err);
 	}
 }
